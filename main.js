0762fa101d4884f58f7f07e83487564213fccc65
Victoria Corona <corona.victoria@hotmail.com>
2020-06-02 17:35:09 -0700

    cards can flip

--- main.js
+++ main.js
@@ -4,4 +4,9 @@ gameCards.addEventListener("click", handleClick);
 
 function handleClick(event) {
   console.log(event);
+
+  if (event.target.className.indexOf("card-back") === -1) {
+    return;
+  }
+  event.target.className += " hidden";
 }
