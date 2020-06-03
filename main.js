ab1897d726c7c118191a2418cb77c33b1622ceac
Victoria Corona <corona.victoria@hotmail.com>
2020-06-02 17:54:26 -0700

    created global variables

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var gameCards = document.getElementById("gameCards");
 
+
 gameCards.addEventListener("click", handleClick);
 
-function handleClick(event) {
-  console.log(event);
 
+function handleClick(event) {
   if (event.target.className.indexOf("card-back") === -1) {
     return;
   }
